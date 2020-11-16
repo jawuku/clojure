a4e88e868f69847815f78ec6d391d8467b99b2b5
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2020-11-16 22:25:37 +0000

    Collatz problem in exercism.io

---
+++ collatz-conjecture.clj
@@ -0,0 +1,24 @@
+(ns collatz-conjecture)
+
+(def steps (atom 0))
+(def steps-return (atom 0))
+
+(defn collatz [n]
+;; only calls function if n != 1
+;; increments the step counter with each iteration
+;; implements recursive calls
+  (if-not (= n 1)
+    (if (even? n)
+      (do
+        (swap! steps inc)
+        (collatz (/ n 2)))
+      (do
+        (swap! steps inc)
+        (collatz (+ (* 3 n) 1))))
+;; stops accumulation of steps in subsequent function calls
+    (do
+      (reset! steps-return @steps)
+      (reset! steps 0)))
+    @steps-return)
+
+(collatz 12)
