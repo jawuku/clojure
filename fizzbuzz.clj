64c2c5e2094e254307886cfec5f8817f7424e010
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2020-11-17 00:02:45 +0000

    Update fizzbuzz.clj

--- fizzbuzz.clj
+++ fizzbuzz.clj
@@ -1,7 +1,13 @@
 (ns fizzbuzz)
 
-(defn fizzbuzz
-  ([] (fizzbuzz 100))
+(defn fb-series
+"Classic FizzBuzz program - enter maximum number to display:
+Displays Fizz for numbers divisible by 3,
+Buzz for numbers divisible by 5,
+FizzBuzz for numbers both divisible by 3 and 5.
+Defaults to 100 if no number specified.
+Format: (fb-series n)"
+  ([] (fb-series 100))
   ([n] 
     (dotimes [i n]  
       (cond
@@ -17,3 +23,21 @@
         :else
         (println (str i))))))
 
+(defn fizzbuzz
+"Displays single number
+according to FizzBuzz rules
+Displays random number (up to 1000) if unspecified.
+Format: (fizzbuzz n)"
+  ([] (fizzbuzz (inc (int (* (rand) 1000)))))
+  ([n] (cond
+         (and (zero? (mod n 3)) (zero? (mod n 5)))
+         (println (str n " - FizzBuzz"))
+
+         (zero? (mod n 3))
+         (println (str n " - Fizz"))
+
+         (zero? (mod n 5))
+         (println (str n " - Buzz"))
+
+         :else
+         (println (str n)))))
