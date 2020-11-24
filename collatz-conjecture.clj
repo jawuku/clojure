089b2169c293210cb79a4bd56c53ffd3b018a3a6
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2020-11-24 04:42:03 +0000

    Update collatz-conjecture.clj
    add exception handling

--- collatz-conjecture.clj
+++ collatz-conjecture.clj
@@ -3,11 +3,28 @@
 (def steps (atom 0))
 (def steps-return (atom 0))
 
+
+(defn number-valid? [n]
+  (cond
+    (not (pos? n))
+    (do
+      (throw (Exception. "Number cannot be 0 or negative. Please try again."))
+    false)
+    
+    (not (integer? n))
+    (do
+      (throw (Exception. "You input a floating point number. Please try again with an integer."))
+    false)
+    
+    :else
+    true))
+   
+
 (defn collatz [n]
 ;; only calls function if n != 1
 ;; increments the step counter with each iteration
 ;; implements recursive calls
-  (if-not (= n 1)
+  (if (and (not= n 1) (number-valid? n))
     (if (even? n)
       (do
         (swap! steps inc)
